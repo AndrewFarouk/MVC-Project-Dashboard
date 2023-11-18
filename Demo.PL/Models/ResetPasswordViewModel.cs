85acfd339e77a0a73f1c4bdc830347571cfca13f
AndrewFarouk <104110420+AndrewFarouk@users.noreply.github.com>
2023-11-18 22:21:10 +0200

    Update ResetPasswordViewModel.cs

--- Demo.PL/Models/ResetPasswordViewModel.cs
+++ Demo.PL/Models/ResetPasswordViewModel.cs
@@ -7,9 +7,11 @@ namespace Demo.PL.Models
         [Required]
         [StringLength(8, MinimumLength = 8, ErrorMessage = "The Password Length Must be 8")]
         public string Password { get; set; }
+        
         [Required]
         [Compare(nameof(Password), ErrorMessage = "Password Mismatch")]
         public string ConfirmPassword { get; set; }
+        
         public string Email { get; set; }
         public string Token { get; set; }
     }
