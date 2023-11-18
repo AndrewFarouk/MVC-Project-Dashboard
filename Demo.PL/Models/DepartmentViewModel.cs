df898946ef78613b06d6618617eef0f94d6d39cb
AndrewFarouk <104110420+AndrewFarouk@users.noreply.github.com>
2023-11-18 22:20:45 +0200

    Update DepartmentViewModel.cs

--- Demo.PL/Models/DepartmentViewModel.cs
+++ Demo.PL/Models/DepartmentViewModel.cs
@@ -5,12 +5,15 @@ namespace Demo.PL.Models
     public class DepartmentViewModel
     {
         public int Id { get; set; }
+        
         [Required(ErrorMessage = "Code Is Required")]
         public string Code { get; set; }
+        
         [Required(ErrorMessage = "Name Is Required")]
         [MinLength(2)]
         [MaxLength(20)]
         public string Name { get; set; }
+        
         public DateTime DateOfCreation { get; set; } = DateTime.Now;
     }
 }
